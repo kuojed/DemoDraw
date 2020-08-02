329add2ce29d3eefb08989d913f7712d6b49d008
kuojed <kuojed@gmail.com>
2020-08-02 13:25:33 +0800

    Initial Commit

---
+++ DemoDraw/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DemoDraw
+//
+//  Created by 郭冠杰 on 2020/8/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
